fe4dda91d092e20f3438e8f366ad8f2071cd9977
Rafael Teixeira <rafael.amtx@gmail.com>
2019-07-26 17:13:47 -0300

    model implement

--- src/main/java/br/com/study4devs/appstudy4devs/model/Student.java
+++ src/main/java/br/com/study4devs/appstudy4devs/model/Student.java
@@ -1,27 +1,25 @@
 package br.com.study4devs.appstudy4devs.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-@Getter
-@Setter
-public class Question {
+public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String title;
-    private int rightAnswer;
-    private String firstAnswer;
-    private String secondAnswer;
-    private String thirdAnswer;
-    private String fourthAnswer;
+    private String name;
+    private String login;
+    private String password;
+    private String email;
+    private int points;
 
+    public int whatsMyRank(){
+        int temp = 0;
+        return temp;
+    }
 }
